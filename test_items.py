58297bd08ca54268d481952ea2096dea1ffb9d73
Kate <r_girl@inbox.ru>
2022-04-02 20:57:56 -0500

    edit with module time

--- test_items.py
+++ test_items.py
@@ -1,13 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_check_button_add_to_basket(browser):
     browser.get(link)
-    button_add_to_basket = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'btn-add-to-basket')))
+    time.sleep(30)
+    button_add_to_basket = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
 
     assert button_add_to_basket, f'Страница товара на сайте {link} не содержит кнопки добавления в корзину'
